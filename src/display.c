a9029c44ce1d25f62c6251c80938216b1d9a7f6d
Guenael Roger <guenael.mail@gmail.com>
2025-11-07 17:53:59 +0100

    removed time inclusion

--- src/display.c
+++ src/display.c
@@ -1,29 +1,27 @@
 #include "constants.h"
-#include "led_com.h"
+
 #include "hall_sensor.h"
+#include "led_com.h"
 
 #include <avr/io.h>
-#include <stdlib.h>
-#include <time.h>
 #include <util/delay.h>
 
-int display_main(){
-  srand(time(NULL));
+void pass_magnet_callback(void) {
+  write_datastreak(0b1111110001111111);
+  _delay_ms(100);
+}
 
+int display_main() {
   setup_hall_sensor();
   setup_led_driver_com();
-  
+
   uint16_t datastreak = ~(0b1111110000111111);
   while (1) { // Main loop
     write_datastreak(datastreak);
-    if(hall_is_triggered()){
+    if (hall_is_triggered()) {
       pass_magnet_callback();
     };
+    pwm(1);
   }
   return 1;
-}
-
-void pass_magnet_callback(void){
-  write_datastreak(0b1111110001111111);
-  _delay_ms(100);
 }
