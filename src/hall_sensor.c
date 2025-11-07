16cea7b1a5898e58a6880d0eff0dc4110afe9eea
Guenael Roger <guenael.mail@gmail.com>
2025-11-07 17:52:43 +0100

    added needed lib to compile

--- src/hall_sensor.c
+++ src/hall_sensor.c
@@ -1,10 +1,14 @@
+#include "constants.h"
+
+#include <avr/io.h>
+
 #define HALL_SENSOR_PIN PD2
 
 void setup_hall_sensor() {
-  DDRD &= ~(1 << HALL_SENSOR_PIN);   // PD2 (entry)
+  DDRD &= ~(1 << HALL_SENSOR_PIN); // PD2 (entry)
 }
 
 int hall_is_triggered() {
   // returns 1 if there is a magnet
-  return !(PIND & (1 << HALL_SENSOR_PIN));  // avtive when LOW
+  return !(PIND & (1 << HALL_SENSOR_PIN)); // active when LOW
 }
