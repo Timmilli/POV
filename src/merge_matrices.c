67f1070defec6d4d3d0d198223c15acdaedafb70
Guenael Roger <guenael.mail@gmail.com>
2025-11-26 16:17:44 +0100

    fixed usage of progmem

--- src/merge_matrices.c
+++ src/merge_matrices.c
@@ -1,9 +1,12 @@
 #include "constants.h"
 
+#include "buffer.h"
 #include "clock_module.h"
 #include "merge_matrices.h"
 #include "quarter_selection.h"
+#include "uart_com.h"
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 
 void merge_matrices(uint16_t mat[NUMBER_OF_POSITIONS], clock_values_t *cv) {
   uint8_t hours_units = cv->hours % 10;
@@ -16,6 +19,7 @@ void merge_matrices(uint16_t mat[NUMBER_OF_POSITIONS], clock_values_t *cv) {
   const uint16_t *mt_quarter = get_mt_quarter(minutes_tens);
 
   for (uint8_t i = 0; i < NUMBER_OF_POSITIONS; i++) {
-    mat[i] = hu_quarter[i] | ht_quarter[i] | mu_quarter[i] | mt_quarter[i];
+    mat[i] = pgm_read_word(hu_quarter[i]) | pgm_read_word(ht_quarter[i]) |
+             pgm_read_word(mu_quarter[i]) | pgm_read_word(mt_quarter[i]);
   }
 }
