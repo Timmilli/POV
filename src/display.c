06ae76fc0adc17349ff8dab4a60633d42520c5b1
ncornet001 <ncornet001@ensc.fr>
2025-11-26 16:20:28 +0100

    Single resolution diplay clock

--- src/display.c
+++ src/display.c
@@ -7,18 +7,10 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-float fabs(float x){
-  if(x < 0){
-    return -x;
-  }else{
-    return x;
-  }
-}
-
-
-uint16_t  draw_fork(float angle,float location, uint8_t length){
+uint16_t  draw_fork(uint32_t angle,uint32_t location, uint8_t length){
+  //Angle in 6 degrees
   uint16_t res = ~0b0;
-    if(fabs(angle-location) < 3){
+    if(angle == location){
       res = ~(res >> length);
       return res;
     }else{
@@ -26,27 +18,28 @@ uint16_t  draw_fork(float angle,float location, uint8_t length){
     }
 }
 
-uint16_t draw_cadran(float angle){
+uint16_t draw_cadran(uint32_t angle){
+  //Angle in 6 degrees
   uint16_t res = 0b001;
-  float angle12 = (float)((int)(angle * 5)%(150))/5;
-  float angle4 = (float)((int)(angle * 5)%(450))/5;
-  angle12 -= 3;
-  angle4 -= 3;
-  if(angle12 < 3){
+  float angle12 = angle % 5;
+  float angle4 = angle % 15;
+
+  if(angle12 == 0 || angle12 == 5){
     res |= 0b011;
   }
-  if(angle4 < 3){
+  if(angle4 == 0 || angle4 == 15){
     res|= 0b0111;
   }
   return res;
 
 }
 
-uint16_t draw_clock(float angle){
+uint16_t draw_clock(uint32_t angle){
+  //Angle in 6 degrees
   uint16_t datastreak = draw_cadran(angle);
-  datastreak |= draw_fork(angle,360-(float)clock_get_seconds()*6,14);
-  datastreak |= draw_fork(angle,360-(float)clock_get_minutes()*6,10);
-  datastreak |= draw_fork(angle,360-(float)(clock_get_hours()%12)*6,6);
+  datastreak |= draw_fork(angle,60-clock_get_seconds(),14);
+  datastreak |= draw_fork(angle,60-clock_get_minutes(),10);
+  datastreak |= draw_fork(angle,60-(clock_get_hours()%12)*5,6);
   return datastreak;
 }
 
@@ -57,10 +50,10 @@ int display_main(){
   setup_led_driver_com();
   uint16_t datastreak = 0b00000001;
   clock_set_time(0,30,4);
-  
+
   while (1) { // Main loop
     clock_update();
-    long angle = get_current_angle();
+    uint32_t angle = (get_current_angle()/6 + 0.5);
     if(alternator){
       datastreak = draw_clock(angle) | 0b1000000;
     }else{
