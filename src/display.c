24fe6f9de303e44162fe0dca4582237c11796fec
ncornet001 <ncornet001@ensc.fr>
2025-11-26 15:52:26 +0100

    Debug alternator for clock

--- src/display.c
+++ src/display.c
@@ -28,8 +28,8 @@ uint16_t  draw_fork(float angle,float location, uint8_t length){
 
 uint16_t draw_cadran(float angle){
   uint16_t res = 0b001;
-  float angle12 = (float)((int)(angle * 5)%(30 * 5))/5;
-  float angle4 = (float)((int)(angle * 5)%(90 * 5))/5;
+  float angle12 = (float)((int)(angle * 5)%(150))/5;
+  float angle4 = (float)((int)(angle * 5)%(450))/5;
   angle12 -= 3;
   angle4 -= 3;
   if(angle12 < 3){
@@ -44,13 +44,14 @@ uint16_t draw_cadran(float angle){
 
 uint16_t draw_clock(float angle){
   uint16_t datastreak = draw_cadran(angle);
-  datastreak |= draw_fork(angle,360-(float)clock_get_seconds()/60*360,14);
-  datastreak |= draw_fork(angle,360-(float)clock_get_minutes()/60*360,10);
-  datastreak |= draw_fork(angle,360-(float)(clock_get_hours()%12)/12*360,6);
+  datastreak |= draw_fork(angle,360-(float)clock_get_seconds()*6,14);
+  datastreak |= draw_fork(angle,360-(float)clock_get_minutes()*6,10);
+  datastreak |= draw_fork(angle,360-(float)(clock_get_hours()%12)*6,6);
   return datastreak;
 }
 
 int display_main(){
+  uint8_t alternator = 0;
   srand(time(NULL));
   setup_hall_sensor();
   setup_led_driver_com();
@@ -60,7 +61,13 @@ int display_main(){
   while (1) { // Main loop
     clock_update();
     long angle = get_current_angle();
-    datastreak = draw_clock(angle);
+    if(alternator){
+      datastreak = draw_clock(angle) | 0b1000000;
+    }else{
+      datastreak = draw_clock(angle);
+    }
+    alternator = ~alternator;
+
     write_datastreak(datastreak);
   }
   return 1;
