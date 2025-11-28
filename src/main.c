eb7dfdcabaa88a3f332bba9abd34138ce7e209e3
Guenael Roger <guenael.mail@gmail.com>
2025-11-28 09:09:08 +0100

    update after moving straight clock main to straight clock file

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "constants.h"
 
 #include "buffer.h"
+#include "classic_clock.h"
 #include "clock_module.h"
 #include "hall_sensor.h"
 #include "led_com.h"
 #include "merge_matrices.h"
+#include "straight_clock.h"
 #include "uart_com.h"
 #include <avr/interrupt.h>
 #include <avr/io.h>
@@ -17,6 +19,8 @@ clock_values_t cv;
 uint16_t datastreak = 0;
 uint16_t mat[NUMBER_OF_POSITIONS];
 uint8_t end_of_string = 0;
+display_mode_e mode = STRAIGHT_CLOCK;
+uint8_t clock_updated = 1;
 
 /**
  * Receiving interrupt function
@@ -62,42 +66,8 @@ int main(void) {
   // Sending a first string
   uart_send_string("\n\nReady!\n", &tx_buffer);
 
-  // Creating the initial clock matrix
-  merge_matrices(mat, &cv);
-
   // Main loop
   while (1) {
-    /*
-     * Creating the datastreak
-     */
-    uint32_t angle = get_current_angle();
-    // Getting the hours and minutes digits
-    if (angle < 360)
-      datastreak = mat[angle / (360 / NUMBER_OF_POSITIONS)];
-    else
-      datastreak = 0;
-
-    // Putting the seconds as the outer led ring
-    uint8_t s = clock_get_seconds(&cv);
-    if (angle > 180) {
-      if (s < (angle - 180) / 6) {
-        datastreak &= ~(0b0000000000000001);
-      } else {
-        datastreak |= 0b0000000000000001;
-      }
-    } else {
-      if (s > 30) {
-        if ((s - 30) < angle / 6) {
-          datastreak &= ~(0b0000000000000001);
-        } else {
-          datastreak |= 0b0000000000000001;
-        }
-      }
-    }
-
-    // Writing the datastreak
-    write_datastreak(datastreak);
-
     /*
      * Processing the UART communication
      */
@@ -114,7 +84,7 @@ int main(void) {
       // Setting another time
     case SET_HOUR: {
       ring_buffer_update_clock(&rx_buffer, &cv);
-      merge_matrices(mat, &cv);
+      clock_updated = 1;
       // Feedback to the user
       clock_to_string(&cv, clock_format_str);
       uart_send_string("Clock set to: ", &tx_buffer);
@@ -132,9 +102,17 @@ int main(void) {
     case GET_SPEED: {
       uint16_t speed = get_turning_speed();
       uart_send_string("Turning speed: ", &tx_buffer);
-      sprintf(blank_format_str, "%d", speed);
+      sprintf(blank_format_str, "%d\n", speed);
       uart_send_string(blank_format_str, &tx_buffer);
-      uart_send_string("\n", &tx_buffer);
+      break;
+    }
+      // Or changing the mode
+    case CHANGE_MODE: {
+      mode = ring_buffer_update_mode(&rx_buffer);
+      // Feedback to the user
+      uart_send_string("Current mode: ", &tx_buffer);
+      sprintf(blank_format_str, "%d\n", mode);
+      uart_send_string(blank_format_str, &tx_buffer);
       break;
     }
     default:
@@ -142,11 +120,22 @@ int main(void) {
     }
 
     /*
+     * Updating the datastreak according to the mode selected
+     */
+    if (mode == CLASSIC_CLOCK)
+      datastreak = 0;
+    else if (mode == STRAIGHT_CLOCK)
+      datastreak = straight_clock_main(mat, &cv, clock_updated);
+    else if (mode == IMAGE)
+      datastreak = 1;
+
+    // Writing the datastreak
+    write_datastreak(datastreak);
+
+    /*
      * Updating the clock values
      */
-    uint8_t clock_updated = clock_update(&cv);
-    if (clock_updated)
-      merge_matrices(mat, &cv);
+    clock_updated = clock_update(&cv);
   }
 
   return 1;
