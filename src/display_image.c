8590668ccbce00afe33fe8b32211d7a687e6723d
Guenael Roger <guenael.mail@gmail.com>
2025-11-28 11:29:04 +0100

    new file to disaply an image

---
+++ src/display_image.c
@@ -0,0 +1,7 @@
+#include "constants.h"
+
+#include "clock_module.h"
+#include <avr/io.h>
+
+void display_digital_clock(uint16_t mat[NUMBER_OF_POSITIONS],
+                           clock_values_t *cv) {}
---
+++ src/display_image.h
@@ -0,0 +1,12 @@
+#ifndef __DISPLAY_IMAGE_H__
+#define __DISPLAY_IMAGE_H__
+
+#include "constants.h"
+
+#include "clock_module.h"
+#include <avr/io.h>
+
+void display_digital_clock(uint16_t mat[NUMBER_OF_POSITIONS],
+                           clock_values_t *cv);
+
+#endif // __DISPLAY_IMAGE_H__
