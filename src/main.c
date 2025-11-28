de2d53952a1d2ff00295d5b76cd5c55b00f0b8f2
Guenael Roger <guenael.mail@gmail.com>
2025-11-28 10:44:33 +0100

    chanaged dispaly to classic clock

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "constants.h"
-#include "led_com.h"
-#include "display.h"
 
-int main(void) { return display_main(); }
+#include "classic_clock.h"
+#include "led_com.h"
+
+int main(void) { return classic_clock_main(); }
